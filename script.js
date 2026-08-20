13b6ed405524e291d8de43213f879a5852b88e67
castillocruzalexjoaquini-spec <castillocruzalexjoaquini@gmail.com>
2026-08-20 01:01:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,67 +8,67 @@ const WHATSAPP = "524612001099";
 
 const productos = [
 
-  { id: 1, name: "Producto Ayzen P1", nombre: "Producto Ayzen P1", price: 250, precio: 250, image: "image/p1.jpg", imagen: "image/p1.jpg", cat: "general", categoria: "general" },
+  { id: 1, name: "Producto Ayzen P1", nombre: "Producto Ayzen P1", price: 250, precio: 250, image: "images/p1.jpg", imagen: "imagess/p1.jpg", cat: "general", categoria: "general" },
 
-  { id: 2, name: "Producto Ayzen P2", nombre: "Producto Ayzen P2", price: 250, precio: 250, image: "image/p2.jpg", imagen: "image/p2.jpg", cat: "general", categoria: "general" },
+  { id: 2, name: "Producto Ayzen P2", nombre: "Producto Ayzen P2", price: 250, precio: 250, image: "images/p2.jpg", imagen: "imagess/p2.jpg", cat: "general", categoria: "general" },
 
-  { id: 3, name: "Producto Ayzen P3", nombre: "Producto Ayzen P3", price: 300, precio: 300, image: "image/p3.jpg", imagen: "image/p3.jpg", cat: "general", categoria: "general" },
+  { id: 3, name: "Producto Ayzen P3", nombre: "Producto Ayzen P3", price: 300, precio: 300, image: "images/p3.jpg", imagen: "images/p3.jpg", cat: "general", categoria: "general" },
 
-  { id: 4, name: "Producto Ayzen P4", nombre: "Producto Ayzen P4", price: 300, precio: 300, image: "image/p4.jpg", imagen: "image/p4.jpg", cat: "general", categoria: "general" },
+  { id: 4, name: "Producto Ayzen P4", nombre: "Producto Ayzen P4", price: 300, precio: 300, image: "images/p4.jpg", imagen: "images/p4.jpg", cat: "general", categoria: "general" },
 
-  { id: 5, name: "Producto Ayzen P5", nombre: "Producto Ayzen P5", price: 550, precio: 550, image: "image/p5.jpg", imagen: "image/p5.jpg", cat: "jeans", categoria: "jeans" },
+  { id: 5, name: "Producto Ayzen P5", nombre: "Producto Ayzen P5", price: 550, precio: 550, image: "images/p5.jpg", imagen: "images/p5.jpg", cat: "jeans", categoria: "jeans" },
 
-  { id: 6, name: "Producto Ayzen P6", nombre: "Producto Ayzen P6", price: 200, precio: 200, image: "image/p6.jpg", imagen: "image/p6.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 6, name: "Producto Ayzen P6", nombre: "Producto Ayzen P6", price: 200, precio: 200, image: "images/p6.jpg", imagen: "images/p6.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 7, name: "Producto Ayzen P7", nombre: "Producto Ayzen P7", price: 250, precio: 250, image: "image/p7.jpg", imagen: "image/p7.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 7, name: "Producto Ayzen P7", nombre: "Producto Ayzen P7", price: 250, precio: 250, image: "images/p7.jpg", imagen: "images/p7.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 8, name: "Producto Ayzen P8", nombre: "Producto Ayzen P8", price: 200, precio: 200, image: "image/p8.jpg", imagen: "image/p8.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 8, name: "Producto Ayzen P8", nombre: "Producto Ayzen P8", price: 200, precio: 200, image: "images/p8.jpg", imagen: "images/p8.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 9, name: "Producto Ayzen P9", nombre: "Producto Ayzen P9", price: 170, precio: 170, image: "image/p9.jpg", imagen: "image/p9.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 9, name: "Producto Ayzen P9", nombre: "Producto Ayzen P9", price: 170, precio: 170, image: "images/p9.jpg", imagen: "images/p9.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 10, name: "Producto Ayzen P10", nombre: "Producto Ayzen P10", price: 220, precio: 220, image: "image/p10.jpg", imagen: "image/p10.jpg", cat: "general", categoria: "general" },
+  { id: 10, name: "Producto Ayzen P10", nombre: "Producto Ayzen P10", price: 220, precio: 220, image: "images/p10.jpg", imagen: "images/p10.jpg", cat: "general", categoria: "general" },
 
-  { id: 11, name: "Producto Ayzen P11", nombre: "Producto Ayzen P11", price: 110, precio: 110, image: "image/p11.jpg", imagen: "image/p11.jpg", cat: "accesorios", categoria: "accesorios" },
+  { id: 11, name: "Producto Ayzen P11", nombre: "Producto Ayzen P11", price: 110, precio: 110, image: "images/p11.jpg", imagen: "images/p11.jpg", cat: "accesorios", categoria: "accesorios" },
 
-  { id: 12, name: "Producto Ayzen P12", nombre: "Producto Ayzen P12", price: 270, precio: 270, image: "image/p12.jpg", imagen: "image/p12.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 12, name: "Producto Ayzen P12", nombre: "Producto Ayzen P12", price: 270, precio: 270, image: "images/p12.jpg", imagen: "images/p12.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 13, name: "Producto Ayzen P13", nombre: "Producto Ayzen P13", price: 230, precio: 230, image: "image/p13.jpg", imagen: "image/p13.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 13, name: "Producto Ayzen P13", nombre: "Producto Ayzen P13", price: 230, precio: 230, image: "images/p13.jpg", imagen: "images/p13.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 14, name: "Producto Ayzen P14", nombre: "Producto Ayzen P14", price: 530, precio: 530, image: "image/p14.jpg", imagen: "image/p14.jpg", cat: "jeans", categoria: "jeans" },
+  { id: 14, name: "Producto Ayzen P14", nombre: "Producto Ayzen P14", price: 530, precio: 530, image: "images/p14.jpg", imagen: "images/p14.jpg", cat: "jeans", categoria: "jeans" },
 
-  { id: 15, name: "Producto Ayzen P15", nombre: "Producto Ayzen P15", price: 500, precio: 500, image: "image/p15.jpg", imagen: "image/p15.jpg", cat: "jeans", categoria: "jeans" },
+  { id: 15, name: "Producto Ayzen P15", nombre: "Producto Ayzen P15", price: 500, precio: 500, image: "images/p15.jpg", imagen: "images/p15.jpg", cat: "jeans", categoria: "jeans" },
 
-  { id: 16, name: "Producto Ayzen P16", nombre: "Producto Ayzen P16", price: 460, precio: 460, image: "image/p16.jpg", imagen: "image/p16.jpg", cat: "jeans", categoria: "jeans" },
+  { id: 16, name: "Producto Ayzen P16", nombre: "Producto Ayzen P16", price: 460, precio: 460, image: "images/p16.jpg", imagen: "images/p16.jpg", cat: "jeans", categoria: "jeans" },
 
-  { id: 17, name: "Producto Ayzen P17", nombre: "Producto Ayzen P17", price: 180, precio: 180, image: "image/p17.jpg", imagen: "image/p17.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 17, name: "Producto Ayzen P17", nombre: "Producto Ayzen P17", price: 180, precio: 180, image: "images/p17.jpg", imagen: "images/p17.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 18, name: "Producto Ayzen P18", nombre: "Producto Ayzen P18", price: 700, precio: 700, image: "image/p18.jpg", imagen: "image/p18.jpg", cat: "jeans", categoria: "jeans" },
+  { id: 18, name: "Producto Ayzen P18", nombre: "Producto Ayzen P18", price: 700, precio: 700, image: "images/p18.jpg", imagen: "images/p18.jpg", cat: "jeans", categoria: "jeans" },
 
-  { id: 19, name: "Producto Ayzen P19", nombre: "Producto Ayzen P19", price: 300, precio: 300, image: "image/p19.jpg", imagen: "image/p19.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 19, name: "Producto Ayzen P19", nombre: "Producto Ayzen P19", price: 300, precio: 300, image: "images/p19.jpg", imagen: "images/p19.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 20, name: "Producto Ayzen P20", nombre: "Producto Ayzen P20", price: 110, precio: 110, image: "image/p20.jpg", imagen: "image/p20.jpg", cat: "accesorios", categoria: "accesorios" },
+  { id: 20, name: "Producto Ayzen P20", nombre: "Producto Ayzen P20", price: 110, precio: 110, image: "images/p20.jpg", imagen: "images/p20.jpg", cat: "accesorios", categoria: "accesorios" },
 
-  { id: 21, name: "Producto Ayzen P21", nombre: "Producto Ayzen P21", price: 110, precio: 110, image: "image/p21.jpg", imagen: "image/p21.jpg", cat: "accesorios", categoria: "accesorios" },
+  { id: 21, name: "Producto Ayzen P21", nombre: "Producto Ayzen P21", price: 110, precio: 110, image: "images/p21.jpg", imagen: "images/p21.jpg", cat: "accesorios", categoria: "accesorios" },
 
-  { id: 22, name: "Producto Ayzen P22", nombre: "Producto Ayzen P22", price: 60, precio: 60, image: "image/p22.jpg", imagen: "image/p22.jpg", cat: "accesorios", categoria: "accesorios" },
+  { id: 22, name: "Producto Ayzen P22", nombre: "Producto Ayzen P22", price: 60, precio: 60, image: "images/p22.jpg", imagen: "images/p22.jpg", cat: "accesorios", categoria: "accesorios" },
 
-  { id: 23, name: "Producto Ayzen P23", nombre: "Producto Ayzen P23", price: 160, precio: 160, image: "image/p23.jpg", imagen: "image/p23.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 23, name: "Producto Ayzen P23", nombre: "Producto Ayzen P23", price: 160, precio: 160, image: "images/p23.jpg", imagen: "images/p23.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 24, name: "Producto Ayzen P24", nombre: "Producto Ayzen P24", price: 120, precio: 120, image: "image/p24.jpg", imagen: "image/p24.jpg", cat: "accesorios", categoria: "accesorios" },
+  { id: 24, name: "Producto Ayzen P24", nombre: "Producto Ayzen P24", price: 120, precio: 120, image: "images/p24.jpg", imagen: "images/p24.jpg", cat: "accesorios", categoria: "accesorios" },
 
-  { id: 25, name: "Producto Ayzen P25", nombre: "Producto Ayzen P25", price: 540, precio: 540, image: "image/p25.jpg", imagen: "image/p25.jpg", cat: "jeans", categoria: "jeans" },
+  { id: 25, name: "Producto Ayzen P25", nombre: "Producto Ayzen P25", price: 540, precio: 540, image: "images/p25.jpg", imagen: "images/p25.jpg", cat: "jeans", categoria: "jeans" },
 
-  { id: 26, name: "Baggy jeans negros bordado sakura rosa + cadenas", nombre: "Baggy jeans negros bordado sakura rosa + cadenas", price: 600, precio: 600, image: "image/p26.jpg", imagen: "image/p26.jpg", cat: "jeans", categoria: "jeans" },
+  { id: 26, name: "Baggy jeans negros bordado sakura rosa + cadenas", nombre: "Baggy jeans negros bordado sakura rosa + cadenas", price: 600, precio: 600, image: "images/p26.jpg", imagen: "images/p26.jpg", cat: "jeans", categoria: "jeans" },
 
-  { id: 27, name: "Playera oversize Freedom Estatua tapándose cara", nombre: "Playera oversize Freedom Estatua tapándose cara", price: 230, precio: 230, image: "image/p27.jpg", imagen: "image/p27.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 27, name: "Playera oversize Freedom Estatua tapándose cara", nombre: "Playera oversize Freedom Estatua tapándose cara", price: 230, precio: 230, image: "images/p27.jpg", imagen: "images/p27.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 28, name: "Cadenas pantalón plateadas multicapa estrella", nombre: "Cadenas pantalón plateadas multicapa estrella", price: 70, precio: 70, image: "image/p28.jpg", imagen: "image/p28.jpg", cat: "accesorios", categoria: "accesorios" },
+  { id: 28, name: "Cadenas pantalón plateadas multicapa estrella", nombre: "Cadenas pantalón plateadas multicapa estrella", price: 70, precio: 70, image: "images/p28.jpg", imagen: "images/p28.jpg", cat: "accesorios", categoria: "accesorios" },
 
-  { id: 29, name: "Playera negra lavada rosa roja minimalista", nombre: "Playera negra lavada rosa roja minimalista", price: 180, precio: 180, image: "image/p29.jpg", imagen: "image/p29.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 29, name: "Playera negra lavada rosa roja minimalista", nombre: "Playera negra lavada rosa roja minimalista", price: 180, precio: 180, image: "images/p29.jpg", imagen: "images/p29.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 30, name: "Playera blanca oversize 25 gótica tribal", nombre: "Playera blanca oversize 25 gótica tribal", price: 200, precio: 200, image: "image/p30.jpg", imagen: "image/p30.jpg", cat: "playeras", categoria: "playeras" },
+  { id: 30, name: "Playera blanca oversize 25 gótica tribal", nombre: "Playera blanca oversize 25 gótica tribal", price: 200, precio: 200, image: "images/p30.jpg", imagen: "images/p30.jpg", cat: "playeras", categoria: "playeras" },
 
-  { id: 31, name: "Baggy jeans negros bordado floral blanco lateral", nombre: "Baggy jeans negros bordado floral blanco lateral", price: 480, precio: 480, image: "image/p31.jpg", imagen: "image/p31.jpg", cat: "jeans", categoria: "jeans" }
+  { id: 31, name: "Baggy jeans negros bordado floral blanco lateral", nombre: "Baggy jeans negros bordado floral blanco lateral", price: 480, precio: 480, image: "images/p31.jpg", imagen: "images/p31.jpg", cat: "jeans", categoria: "jeans" }
 
 ];
 
